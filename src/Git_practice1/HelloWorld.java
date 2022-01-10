17d5b5f388d2ebeb3ce2197bcc1ba053363d7c7e
IrinaOrli <93168720+IrinaOrli@users.noreply.github.com>
2022-01-10 15:12:15 -0500

    work ********************

--- src/Git_practice1/HelloWorld.java
+++ src/Git_practice1/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
         // Doing awesome work 3 !
         // Awesome work 3 is done and ready to be committed
         // and ready to be pushed to remote repository
+        // 888888**************
 
     }
 }
