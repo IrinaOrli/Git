e16c7b922cf79159c775ab14a35f7637d65a852c
IrinaOrli <93168720+IrinaOrli@users.noreply.github.com>
2022-01-11 11:49:07 -0500

    TestCase001 change 2 made

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -4,5 +4,6 @@ public class TestCase001 {
     public static void main(String[] args) {
         System.out.println("Main method created");
         System.out.println("change 1 made");
+        System.out.println("change 2 made");
     }
 }
